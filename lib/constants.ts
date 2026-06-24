579ff694d288984858cbe6982d08a00ceb386191
kartavyagore <kartavyagore0@gmail.com>
2026-06-24 21:39:12 +0530

    backend API URL Switch added

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "https://healthjobsnow-backend-latest.onrender.com";
+  process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8080";
 
 export const TOKEN_KEY = "hjn_token";
 export const USER_KEY = "hjn_user";
